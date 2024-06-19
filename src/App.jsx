78ee07c31ffc63cc5fcb5e2cc8f1e030e09b638d
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-06-19 04:17:16 +0000

    nahloh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import React from 'react'
+import { Route, Router } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 function App() {
-
-  return (
-    <>dudi
-    </>
-  )
+  return <>
+    <Router>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/auth' element={<AuthPage/>}/>
+    </Router>
+  </>
 }
 
 export default App
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <div>halo tamu</div>
   )
 }
 
