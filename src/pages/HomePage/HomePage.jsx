b7d7c17e9045e0bf936ee145039b8483989f710e
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-06-19 11:21:05 +0000

    dahlah

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
+import { Button, Flex } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-  return (
-    <div>Selamat datang diberanda</div>
-  )
+  return  <>
+    <Flex>Selamat datang diberanda</Flex>
+    <Link to={"/auth"}>babay</Link>
+    <Button>Test</Button>
+  </>
 }
 
 export default HomePage
