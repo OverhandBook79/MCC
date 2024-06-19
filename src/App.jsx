eb2ca8318f7c63b2cd5076a234ae010539a3cb5f
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-06-19 04:33:54 +0000

    yahaha

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Route, Router } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import AuthPage from './pages/AuthPage/AuthPage'
 
 function App() {
   return <>
-    <Router>
+    <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/auth' element={<AuthPage/>}/>
-    </Router>
+    </Routes>
   </>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
