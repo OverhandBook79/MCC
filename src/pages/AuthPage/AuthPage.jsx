ee56b08c883f1b89044ee17721bdf56545f22801
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-06-20 08:22:55 +0000

    nahkan

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,19 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 const AuthPage = () => {
   return (
-    <div>halo tamu</div>
+    <Flex minH={"100vh"} px={4} justifyContent={"center"} alignItems={"center"}>
+      <Container p={0} maxW={"container.md"}>
+        <Box display={{ base: "none", md: "block"}}>
+
+        </Box>
+        <Box>
+
+        </Box>
+
+      </Container>
+    </Flex>
   )
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
+  
   return ( <>
     <Flex>Selamat datang diberanda</Flex>
     <Link to={"/auth"}>babay</Link>
