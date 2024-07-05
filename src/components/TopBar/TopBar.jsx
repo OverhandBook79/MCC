136469d4d11b94aef147b9bad5c3e265aa822625
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-07-05 06:55:06 +0000

    nah

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,6 +1,6 @@
 // src/components/TopBar.jsx
 import React from 'react';
-import { Flex, Button, Avatar, Text, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, DrawerCloseButton, VStack, Input, useColorModeValue, Link as ChakraLink, useColorMode, Link, Image } from '@chakra-ui/react';
+import { Flex, Button, Avatar, Text, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, DrawerCloseButton, VStack, Input, useColorModeValue, Link as ChakraLink, useColorMode, Link, Image, IconButton } from '@chakra-ui/react';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link as RouterLink } from 'react-router-dom';
 import { CiLogout } from "react-icons/ci";
@@ -9,9 +9,10 @@ import { TbWorld } from "react-icons/tb";
 import { AiOutlineSkin } from "react-icons/ai";
 import { IoExtensionPuzzleOutline } from 'react-icons/io5';
 import { HiOutlineHome } from "react-icons/hi2";
+import { FaGithub, FaMoon, FaSun, FaYoutube } from 'react-icons/fa';
 
 const TopBar = ({ user, onLogin, onLogout }) => {
-  const { colorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const logoSrc = colorMode === 'light' ? '/logo-light.png' : '/logo-dark.png';
   const bgColor = useColorModeValue("gray.50", "gray.900");
@@ -19,48 +20,63 @@ const TopBar = ({ user, onLogin, onLogout }) => {
   return (
     <Flex justifyContent="space-between" p={4} borderBottom="1px" borderColor="gray.200" bgColor={bgColor} gap={2}>
       <ChakraLink as={Link} to="/">
-          <Image src={logoSrc} h={10} display={{ base: "none", sm: "block" }} cursor="pointer" />
+        <Image src={logoSrc} h={10} w={45} cursor="pointer" />
       </ChakraLink>
       <Input placeholder="Search" width="full" />
-        <>
-          <Button onClick={onOpen}><RxHamburgerMenu /></Button>
-          <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-            <DrawerOverlay />
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerHeader>Menu</DrawerHeader>
-              <DrawerBody>
-                <VStack spacing={4}>
-                  {user ? (
-                    <>
-                    <Button as={RouterLink} to={'/profile'} onClick={onClose} w={"100%"} gap={2}>
-                        <Flex alignItems="center">
-                          <Avatar name={user.username} src={user.avatar} size="sm" cursor="pointer" />
-                          <Text ml={2} isTruncated maxWidth="200px">{user.username}</Text>
-                        </Flex>
+      <>
+        <Button onClick={onOpen}><RxHamburgerMenu /></Button>
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <VStack spacing={4}>
+                {user ? (
+                  <>
+                    <Button as={Link} to={'/profile'} onClick={onClose} w={"100%"} gap={2}>
+                      <Flex alignItems="center">
+                        <Avatar name={user.username} src={user.avatar} size="sm" cursor="pointer" />
+                        <Text ml={2} isTruncated maxWidth="200px">{user.username}</Text>
+                      </Flex>
                     </Button>
-                      <Button as={RouterLink} to="/" onClick={onClose} w="100%" gap={2}><HiOutlineHome /> Home</Button>
-                      <Button as={RouterLink} to="/addons" onClick={onClose} w="100%" gap={2}><IoExtensionPuzzleOutline /> Addons</Button>
-                      <Button as={RouterLink} to="/worlds" onClick={onClose} w="100%" gap={2}><BiWorld/> Worlds</Button>
-                      <Button as={RouterLink} to="/skins" onClick={onClose} w="100%" gap={2}><AiOutlineSkin /> Skins</Button>
-                      <Button as={RouterLink} to="/servers" onClick={onClose} w="100%" gap={2}><TbWorld /> Servers</Button>
-                      <Button w="100%" onClick={onLogout} gap={2} ><CiLogout /> Logout</Button>
-                    </>
-                  ) : (
-                    <>
+                    <Button as={Link} to="/" onClick={onClose} w="100%" gap={2}><HiOutlineHome /> Home</Button>
+                    <Button as={Link} to="/addons" onClick={onClose} w="100%" gap={2}><IoExtensionPuzzleOutline /> Addons</Button>
+                    <Button as={Link} to="/worlds" onClick={onClose} w="100%" gap={2}><BiWorld /> Worlds</Button>
+                    <Button as={Link} to="/skins" onClick={onClose} w="100%" gap={2}><AiOutlineSkin /> Skins</Button>
+                    <Button as={Link} to="/servers" onClick={onClose} w="100%" gap={2}><TbWorld /> Servers</Button>
+                    <Button w="100%" onClick={onLogout} gap={2} ><CiLogout /> Logout</Button>
+                  </>
+                ) : (
+                  <>
                     <Button w="100%" onClick={onLogin}>Login</Button>
-                    <Button as={RouterLink} to="/" onClick={onClose} w="100%" gap={2}><HiOutlineHome /> Home</Button>
-                    <Button as={RouterLink} to="/addons" onClick={onClose} w="100%" gap={2}><IoExtensionPuzzleOutline /> Addons</Button>
-                    <Button as={RouterLink} to="/worlds" onClick={onClose} w="100%" gap={2}><BiWorld/> Worlds</Button>
-                    <Button as={RouterLink} to="/skins" onClick={onClose} w="100%" gap={2}><AiOutlineSkin /> Skins</Button>
-                    <Button as={RouterLink} to="/servers" onClick={onClose} w="100%" gap={2}><TbWorld /> Servers</Button>
-                    </>
-                  )}
-                </VStack>
-              </DrawerBody>
-            </DrawerContent>
-          </Drawer>
-        </>
+                    <Button as={Link} to="/" onClick={onClose} w="100%" gap={2}><HiOutlineHome /> Home</Button>
+                    <Button as={Link} to="/addons" onClick={onClose} w="100%" gap={2}><IoExtensionPuzzleOutline /> Addons</Button>
+                    <Button as={Link} to="/worlds" onClick={onClose} w="100%" gap={2}><BiWorld /> Worlds</Button>
+                    <Button as={Link} to="/skins" onClick={onClose} w="100%" gap={2}><AiOutlineSkin /> Skins</Button>
+                    <Button as={Link} to="/servers" onClick={onClose} w="100%" gap={2}><TbWorld /> Servers</Button>
+                  </>
+                )}
+                <Flex gap={2} justifyContent="center">
+                  <Link href="https://github.com/OverhandBook79" isExternal>
+                    <IconButton>
+                      <FaGithub />
+                    </IconButton>
+                  </Link>
+                  <Link href="https://youtube.com/@overhandbook79?si=sy6-m4INPuUcq1Ia" isExternal>
+                    <IconButton>
+                      <FaYoutube />
+                    </IconButton>
+                  </Link>
+                  <IconButton onClick={toggleColorMode}>
+                    {colorMode === 'light' ? <FaMoon /> : <FaSun />}
+                  </IconButton>
+                </Flex>
+              </VStack>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </>
     </Flex>
   );
 };
