3d031bf869fe5b2977debf023f0f5cf4771d6d69
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-07-13 13:54:04 +0000

    #

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: 'src/main.jsx',
-    },
+    outDir: 'dist',
   },
 });
