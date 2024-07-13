a9ea6df79ce5d71a1471b67d9e0d73f6fef82603
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-07-13 05:28:28 +0000

    nah

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrdgGvm91DlzGDYzWDq6tvIxXRRsVoXV4",
-  authDomain: "mine-crafter-community.firebaseapp.com",
-  projectId: "mine-crafter-community",
-  storageBucket: "mine-crafter-community.appspot.com",
-  messagingSenderId: "999518187868",
-  appId: "1:999518187868:web:2e854b5f677cb6886a9199",
-  measurementId: "G-F7TQX0JR61"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
