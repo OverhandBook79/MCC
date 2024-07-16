fee765d02a97ac77abaf337d0eaef9407cb142ee
OverhandBook79 <150416843+OverhandBook79@users.noreply.github.com>
2024-07-16 15:26:52 +0000

    #

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,31 +1,36 @@
-import { Box, Center, Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
-import React from 'react'
-import SignIn from '../../components/ProfileModal/SignIn'
-import SignUp from '../../components/ProfileModal/SignUp'
+import { Box, Center, Flex, Tab, TabList, TabPanel, TabPanels, Tabs, useColorModeValue } from '@chakra-ui/react';
+import React from 'react';
+import SignIn from '../../components/ProfileModal/SignIn';
+import SignUp from '../../components/ProfileModal/SignUp';
 
 const AuthPage = () => {
-  return (
-    <>
-      <Flex alignItems={'center'} justifyContent={'center'} w={'full'} h='full'>
-        <Box w={'300px'}>
-      <Tabs size='md' variant='enclosed' w={300} h='full'>
-        <TabList w={300}>
-          <Tab>Sign Up</Tab>
-          <Tab>Sign In</Tab>
-        </TabList>
-        <TabPanels w={300}>
-          <TabPanel w={300}>
-            <SignUp w={300}/>
-          </TabPanel >
-          <TabPanel w={300}>
-            <SignIn w={300}/>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-      </Box>
-      </Flex>
-    </>
-  )
-}
+  const bgColor = useColorModeValue("white", "gray.800");
 
-export default AuthPage
+  return (
+    <Flex alignItems="center" justifyContent="center" w="100vw" h="100vh" bgColor={bgColor}>
+      <Box
+        w="350px"
+        borderRadius="lg"
+        overflow="hidden"
+        boxShadow="lg"
+      >
+        <Tabs size="md" variant="enclosed">
+          <TabList>
+            <Tab>Sign Up</Tab>
+            <Tab>Sign In</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+            <TabPanel>
+              <SignIn />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Flex>
+  );
+};
+
+export default AuthPage;
